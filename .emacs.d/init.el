92b0c9b96b2ae4c7810961bdef8e5c631e380b3e
sancha <miltus31@gmail.com>
2022-02-08 15:01:54 +0530

    adding blacken and autopep8

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,8 @@
 ;; INSTALL PACKAGES
 ;; --------------------------------------
 
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
 (require 'package)
 
 (add-to-list 'package-archives
@@ -16,6 +18,9 @@
 (defvar myPackages
   '(better-defaults
     elpy
+    flycheck
+    py-autopep8
+    blacken
     material-theme))
 
 (mapc #'(lambda (package)
@@ -32,13 +37,27 @@
 (elpy-enable)
 (ido-mode t)
 
+;; Enable Flycheck
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Enable autopep8
+
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+
+
+(setq elpy-rpc-python-command "python3")
+
 ;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(material-theme elpy better-defaults)))
+ '(package-selected-packages (quote (material-theme elpy better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
